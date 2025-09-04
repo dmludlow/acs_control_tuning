37222801cf40554e362eae2afee682f23982b6c0
Daniel Ludlow <dmludlow@ucla.edu>
2025-09-03 19:42:36 -0700

    Initial commit

---
+++ PD_tuning.m
@@ -0,0 +1,7 @@
+%{
+This file contains functionality to facilitate PD controller gain tuning
+based on given initial and desired states and other physical properties
+and assumptions
+%}
+
+% code goes here...
